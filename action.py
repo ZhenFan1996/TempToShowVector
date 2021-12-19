53ebafc1e71be6ec339b86d590901c19a5e4b387
Zhen Fan <ubumh@student.kit.edu>
2021-12-19 20:51:48 +0100

    add connection

--- action.py
+++ action.py
@@ -5,26 +5,50 @@ from PIL import Image
 import os
 from anki_vector.util import degrees
 import environment as env
+import threading
+import time
 
 
-def turn_around(robot):
-    for i in range(4):
-        robot.behavior.turn_in_place(degrees(90*i))
+class env_Thread(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):                 
+       env.init()
 
+class robot_Thread(threading.Thread):
+    def __init__(self, threadID, name, counter):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.counter = counter
+    def run(self):                 
+       robot_init()
 
-with anki_vector.AsyncRobot(enable_nav_map_feed=True) as robot:
-    robot.camera.init_camera_feed()
-    #latest_nav_map = robot.nav_map.latest_nav_map
-    #robot.motors.set_wheel_motors(50, 50)
-    while True:
+def robot_init():
+   with anki_vector.AsyncRobot(enable_nav_map_feed=True) as robot:
+      robot.camera.init_camera_feed()
+      robot.motors.set_wheel_motors(50, 50)
+      x =1
+      while True:        
         proximity_data = robot.proximity.last_sensor_reading
-        if proximity_data is not None:
-           print('Proximity distance: {0}'.format(proximity_data.distance))
         photo = robot.camera.latest_image
         image = photo.raw_image
         img = np.array(image)       
-        cv2.imshow('img',img)
-        cv2.waitKey(2)
+        cv2.imshow('img',img)     
+        env.follow((x,0))
+        x+=1
+        cv2.waitKey(10)
+
+
+if __name__ == '__main__':
+  env_thread = env_Thread(1,'env',1)
+  robot_Thread = robot_Thread(2,'robot',2)
+
+  env_thread.start()
+  robot_Thread.start()
 
 
 
--- environment.py
+++ environment.py
@@ -2,7 +2,8 @@ import pygame
 import os
 import numpy as np
 import random
-import sys 
+import sys
+import threading
 
 BLACK = np.array((0,0,0))
 WHITE = np.array((200,200,200))
@@ -12,6 +13,15 @@ RED = np.array((255,0,0))
 window_height = 600
 window_width = 600
 matz = np.zeros([window_height//10,window_width//10])
+flag = False
+aim  = None
+
+
+def Event_Def():
+     MOVE_EVENT = pygame.USEREVENT +1
+     if flag == True:
+       move_event =pygame.event.Event(MOVE_EVENT,{"aim_position": aim})
+       pygame.event.post(move_event)
 
 
 def main():
@@ -22,7 +32,7 @@ def main():
     window.fill(GREY)
     drawGrid()
     ori = (0,0)
-    while True:       
+    while True:    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -31,10 +41,33 @@ def main():
         run(ori,aim)
         ori = aim
         pygame.display.flip()
-        clock.tick(80)
+        clock.tick(5)
 
-      
+def init():
+    global window, clock,ori,flag
+    pygame.init()
+    window = pygame.display.set_mode((window_height, window_width))
+    clock = pygame.time.Clock()
+    ori = (0,0)
+    window.fill(GREY)
+    drawGrid()
+    while True:
+        MOVE_EVENT = pygame.USEREVENT +1
+        if flag == True:
+          move_event =pygame.event.Event(MOVE_EVENT,{"aim_position": aim})
+          pygame.event.post(move_event)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == MOVE_EVENT:
+                run(ori,event.aim_position)
+                ori = event.aim_position
+                flag = False
 
+        pygame.display.flip()
+        clock.tick(10)     
+    
 
 def drawGrid():
     blockSize = 10 
@@ -59,7 +92,13 @@ def run(ori,aim):
      pygame.draw.rect(window,RED,aime)
      pygame.draw.rect(window, WHITE, rect2, 1)
      pygame.draw.rect(window, WHITE, rect1, 1)
-   
+
+
+def follow(toGo):
+    global flag,aim
+    aim = toGo
+    flag = True
+  
 
 def randomRun(ori):
     if(ori[0]==0):
