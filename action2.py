e166c6d8b281e3fb5b223861bb507cbbb31449c5
Zhen Fan <ubumh@student.kit.edu>
2022-01-27 16:12:49 +0100

    traverse change

--- action2.py
+++ action2.py
@@ -9,7 +9,7 @@ import asyncio
 import keyboard
 
 robot = anki_vector.AsyncRobot(serial= '008014c1',enable_nav_map_feed= True)
-speed =  75
+speed =  60
 offset = 100
 path_dis = 300
 last_time = time.time()
@@ -18,11 +18,22 @@ stack = []
 
 class node():
     def __init__(self,x,y,degree):
+        self.x = x
+        self.y = y 
         self.pos = (x,y)
         self.canGo = [0,0,0,0]
         self.angle_z = Angle(degrees = degree)
-        self.pose = Pose(x=x,y=y,z=0,angle_z = Angle(degrees = degree))
+        self.pose = robot.pose
         self.type = 0
+        self.dirPose = [None] *4
+
+    def isInAreaNode(self,position):
+        x = position[0]
+        y = position[1]
+        if x > self.pos[0] -50 and x<self.pos[0]+50 and y >self.pos[1] -50 and y<self.pos[1]+50:
+            return True
+        return False
+
 
 def dir_correct():
     dir = get_dir()
@@ -51,6 +62,7 @@ def dir_correct():
        turn_future.result()
        move_future = robot.motors.set_wheel_motors(speed,speed,speed*4,speed*4)
        move_future.result()
+       last_time = time.time()
 
 def observe_dfs():
     global last_time
@@ -80,7 +92,10 @@ def pathExist():
        return True
     return False
 
-    
+
+def stack_print():
+    for n in stack:
+        print(n.pos)
 
 def dir_print(dir):
     if dir == 0:
@@ -93,42 +108,45 @@ def dir_print(dir):
        return '左'
 
 def check(forward_obstacle):
-     if not forward_obstacle:
-         print('时间到了确认')
-     else:
-         print('发现障碍确认')
      pose = robot.pose
      n = node(pose.position.x,pose.position.y,pose.rotation.angle_z.degrees)
      print(n.pos,n.angle_z.degrees)
      if not forward_obstacle:
        robot.motors.stop_all_motors()
        proximity_data = robot.proximity.last_sensor_reading
-       print('当前离障碍距离为',proximity_data.distance.distance_mm)
+       #print('当前离障碍距离为',proximity_data.distance.distance_mm)
        if proximity_data.distance.distance_mm < offset:
            n.canGo[get_dir()] = 0
        else:
            n.canGo[get_dir()] = 1     
      count = 0
-     pose = robot.pose
-     right = robot.behavior.turn_in_place(degrees(-90),speed= degrees(90))
+     right = robot.behavior.turn_in_place(degrees(-90),speed =Angle(degrees=90))
+     time.sleep(0.1)
      right.result()
+     time.sleep(0.1)
+     print('此时朝向右方,机器人方向为',dir_print(get_dir()),robot.pose.rotation.angle_z.degrees)
      if pathExist():
          dir = get_dir()
-         n.canGo[dir] = 1
+         n.canGo[dir] = 1 
          print('右方有路,绝对方向为',dir_print(dir))
          count+=1
-     left = robot.behavior.turn_in_place(degrees(180),speed= degrees(90))
+     left = robot.behavior.turn_in_place(degrees(180),speed =Angle(degrees=90))
+     time.sleep(0.1)
      left.result()
+     time.sleep(0.1)
+     print('此时朝向左方,机器人方向为',dir_print(get_dir()),robot.pose.rotation.angle_z.degrees)
      if pathExist():
          dir = get_dir()
          n.canGo[dir] = 1
          print('左方有路，绝对方向为',dir_print(dir))
          count+=1
-     back = robot.behavior.turn_in_place(degrees(-90),speed= degrees(90))
+     back = robot.behavior.turn_in_place(degrees(-90),speed =Angle(degrees=90))
      back.result()
      if count > 0 :
          n.type=1
          stack.append(n)
+         print('该点入栈')
+         stack_print()        
      return n
 
 def base_on_canGo_dir(n):
@@ -137,11 +155,38 @@ def base_on_canGo_dir(n):
             return i
     return -1
 
+def go_to_node(ori,aim):
+    print((aim.x,aim.y),(ori.x,ori.y),'开始节点回溯')
+    diffX = aim.x - ori.x
+    diffY = aim.y - ori.y
+    toDir = -1
+    if abs(diffY)>abs(diffX):
+       if diffY >0 :
+           toDir = 3
+       else:
+           toDir = 1
+    else:
+        if diffX >0:
+           toDir = 0
+        else:
+           toDir = 2
+    turn_future =robot.behavior.turn_in_place(degrees(-90*(toDir-get_dir())))
+    turn_future.result()
+    pose = robot.pose
+    future = robot.motors.set_wheel_motors(speed,speed,speed*4,speed*4)
+    future.result()
+    while abs(pose.position.x-aim.pos[0])>100 or abs(pose.position.y-aim.pos[1])>100:
+        pose = robot.pose
+        print(pose.position.x_y_z,'目标为',(aim.x,aim.y))
+    print('到达目标节点')
+    robot.motors.stop_all_motors()
+
 def move():
     global last_time
     keyboard.wait('a')
     #future = robot.motors.set_wheel_motors(speed,speed,speed*4,speed*4)
     #future.result()
+    is_go_back = False
     lift_future =robot.behavior.set_lift_height(MAX_LIFT_HEIGHT_MM)
     lift_future.result()
     while True:
@@ -155,20 +200,26 @@ def move():
            pri_dir = base_on_canGo_dir(n)
            if pri_dir !=-1:
              dir = get_dir() 
-             future = robot.behavior.turn_in_place(degrees(-90*(pri_dir-dir)),speed= degrees(90))
+             future = robot.behavior.turn_in_place(degrees(-90*(pri_dir-dir)),speed =Angle(degrees=90))
              future.result()
              n.canGo[get_dir()] =-1
-             if n.type ==1:
+             if n.type ==1 and is_go_back:
                  stack.append(n)
+                 print('该点入栈')
+                 stack_print()
              n = None
+             is_go_back = False
            else:
+             ori = n
              if n==stack[-1]:
                  stack.pop()
              n = stack.pop()
              go_future = robot.behavior.go_to_pose(n.pose)
              go_future.result()
+             #go_to_node(ori,n)
+             is_go_back = True
              lift_future =robot.behavior.set_lift_height(MAX_LIFT_HEIGHT_MM)
-             lift_future.result()      
+             lift_future.result()
           last_time = time.time()
           future = robot.motors.set_wheel_motors(speed,speed,speed*4,speed*4)
           future.result()
